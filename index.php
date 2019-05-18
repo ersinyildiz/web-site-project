5b2eab6ec73658e689c7a414a569ef691d57d088
ersinyildiz <ahmetersinyildiz@gmail.com>
2019-05-18 19:24:45 +0300

    header-and-footer-vol1

---
+++ index.php
@@ -0,0 +1,81 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+
+    <title>Kişisel Web Sitem</title>
+</head>
+<body>
+    <div class="page">
+        <div id="header">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="#">Ersin YILDIZ</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="index.php">Anasayfa <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="about.php">Hakkımda</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="hobbies.php">Hobilerim</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="phobia.php">Fobilerim</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="schools.php">Bitirdiğim Okullar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="projects.php">Bitirdiğim Projeler</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.php">İletişim</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            
+            </nav>
+        </div>
+        <div id="content">
+
+        </div>
+        <div id="footer-area">
+            <div class="container footer-inner">
+                <div class="row">
+
+                </div>
+            </div>
+            <footer id="site-footer">
+                <div class="site-info container">
+                    <div class="row">
+                        <nav id="social-menu" style="margin:auto;">
+                            <ul id="social-menu-items">
+                                <li>
+                                    <a href="#"> <img src="images/icon-twitter-48.png"> </a>   
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/icon-instagram-48.png"> </a> 
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/icon-facebook-48.png"> </a> 
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+</body>
+</html>
